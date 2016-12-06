16abb180be1c7262b6c3ac4cac497f782006cf30
ADETUNJI HABEEB <astonish1993@gmail.com>
2016-12-06 16:33:19 +0330

    MyAssignment

---
+++ src/assignment/habeeb.java
@@ -0,0 +1,11 @@
+package assignment;
+
+/**
+ * Created by ADETUNJI HABEEB on 12/6/2016.
+ */
+public class habeeb {
+    public static void main(String[]args)
+    {
+        System.out.println("My Name is ADETUNJI HABEEB ADEYEMI");
+    }
+}
